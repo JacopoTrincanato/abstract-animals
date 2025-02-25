530cf5ba8063d3bb81aac099ec15b4474ef3e5c7
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-25 16:24:21 +0100

    implement Delfino class

---
+++ abstract-animals/src/org/lessons/java/Delfino.java
@@ -0,0 +1,20 @@
+package org.lessons.java;
+
+public class Delfino extends Animale {
+    // costruttore
+    public Delfino(String verso, String cibo) {
+        // richiamo il costruttore della superclasse
+        super(verso, cibo);
+    }
+
+    // implemento i metodi astratti
+    @Override
+    public void verso() {
+        System.out.println("Il delfino fa il seguente verso: " + this.verso);
+    }
+
+    @Override
+    public void mangia() {
+        System.out.println("Il delfino mangia: " + this.cibo);
+    }
+}
