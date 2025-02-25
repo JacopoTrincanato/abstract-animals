973dbfef0974c4f9b49f21a84203d12d1bb10a94
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-25 17:13:09 +0100

    implement faiVolare and faiNuotare methods and test them

--- abstract-animals/src/org/lessons/java/Main.java
+++ abstract-animals/src/org/lessons/java/Main.java
@@ -6,13 +6,31 @@ public class Main {
         // creo i vari animali
         Animale cane = new Cane("abbaiare", "crocchette");
         Animale passerotto = new Passerotto("cinguettare", "vermi");
-        Animale aquila = new Aquila("strido", "carne");
+        Animale aquila = new Aquila("strido", "mammiferi");
         Animale delfino = new Delfino("scoppiettare", "pesce");
+
+        // creo animali che volano o nuotano
+        IVolante aquilaVolante = new Aquila("strido", "uccelli");
+        INuotante caneNuotante = new Cane("abbaiare", "carne");
 
         // testo i vari metodi
         cane.verso();
         passerotto.mangia();
         aquila.dormi();
         delfino.mangia();
+
+        // testo i metodi implementati nella classe Main
+        faiVolare(aquilaVolante);
+        faiNuotare(caneNuotante);
     }
+
+    // aggiungo i metodi statici faiVolare e faiNuotare
+    public static void faiVolare(IVolante animale) {
+        animale.vola();
+    }
+
+    public static void faiNuotare(INuotante animale) {
+        animale.nuota();
+    }
+
 }
