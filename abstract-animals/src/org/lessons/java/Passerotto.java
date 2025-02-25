ce4a26d70e958dd1c74f0bc0f119b23e25f86ad4
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-25 16:23:38 +0100

    implement Paaserotto class and Aquila class

--- abstract-animals/src/org/lessons/java/Passerotto.java
+++ abstract-animals/src/org/lessons/java/Passerotto.java
@@ -1,9 +1,8 @@
 package org.lessons.java;
 
-public class Cane extends Animale {
-
+public class Passerotto extends Animale {
     // costruttore
-    public Cane(String verso, String cibo) {
+    public Passerotto(String verso, String cibo) {
         // richiamo il costruttore della superclasse
         super(verso, cibo);
     }
@@ -11,12 +10,11 @@ public class Cane extends Animale {
     // implemento i metodi astratti
     @Override
     public void verso() {
-        System.out.println("Il cane fa il seguente verso: " + this.verso);
+        System.out.println("Il passerotto fa il seguente verso: " + this.verso);
     }
 
     @Override
     public void mangia() {
-        System.out.println("Il cane mangia: " + this.cibo);
+        System.out.println("Il passerotto mangia: " + this.cibo);
     }
-
 }
